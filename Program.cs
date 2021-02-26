172f0bffc0c3e8c4aba90107497b2635c708e6c9
Xine <cjdoza@gmail.com>
2021-02-26 13:10:39 -0600

    intro file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+
+
+
+// System.Console.Write("Who would you like to say hello to? ");
+
+// string name = System.Console.ReadLine();
+
+// if (string.IsNullOrWhiteSpace(name))
+// {
+//     System.Console.WriteLine("Fine, don't say 'hello'!");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Hello, {name}!");
+// }
